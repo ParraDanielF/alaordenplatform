7b20f4b7960d75bf9ebb4f290232e2d494ed9f0e
ParraDanielF <daniel.parra@chiper.co>
2020-09-30 22:27:55 -0500

    was added the api base project

---
+++ alaordenAPI/src/main/java/com/alaorden/co/AlaordenApiApplication.java
@@ -0,0 +1,13 @@
+package com.alaorden.co;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlaordenApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlaordenApiApplication.class, args);
+	}
+
+}
---
+++ alaordenAPI/src/test/java/com/alaorden/co/AlaordenApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alaorden.co;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlaordenApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
